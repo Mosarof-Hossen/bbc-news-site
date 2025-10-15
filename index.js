57ad3b5cffc8c6add5a488e7bf91865534a355fa
Mosarof-Hossen <mmd58553@gmail.com>
2025-10-15 09:59:11 +0600

    category menu added

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
+const categoryContainer=document.getElementById('categorey-container')
+
+
+
 const loadCategory= ()=>{
     fetch('https://news-api-fs.vercel.app/api/categories')
     .then((res)=> res.json())
-    .then((data)=> console.log(data))
+    .then((data)=> {
+        // console.log(data.categories)
+        const allCategoryes=data.categories
+        allCategoryes.forEach(element => {
+            categoryContainer.innerHTML +=`
+            <li id="${element.id}" class="py-2 hover:border-b-4 hover:border-red-600 cursor-pointer ">${element.title}</li>
+            `
+            
+        });
+        categoryContainer.addEventListener('click',(e)=>{
+            console.log(e)
+            // const allLi=document.querySelectorAll("li")
+            // allLi.forEach(li=>{
+            //     li.classList.remove("border-b-4")
+            // })
+            // if(e.target.localName=== 'li'){
+            //     e.target.classList.add('border-b-4')
+            // }
+
+            const allUlLi=document.querySelectorAll('li')
+            allUlLi.forEach(li=>{
+                li.classList.remove('')
+            })
+            if(e.target.localName==='li'){
+                e.target.classList.add('border-b-4')
+            }
+        })
+
+    })
 
     .catch( (err)=>{console.log(err)})
 }
+
+loadCategory()
 const categoryLod=()=>{
 
 }
